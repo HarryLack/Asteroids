7d033ec29a02d95a32c8bb4e7eee805e0990dafe
Harry B-L <harrylackdev@gmail.com>
2025-03-01 19:21:12 +0000

    chore: pause button

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, UI_COLOUR
 from player import Player
 from asteroidfield import AsteroidField
 from groups import updatable, drawable, asteroids, shots
@@ -26,39 +26,50 @@ def main():
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     _ = AsteroidField()
 
+    paused = False
     # Game Loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        for u in updatable:
-            u.update(dt)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    paused = not paused
 
-        for a in asteroids:
-            if a.collides(player):
-                print(f"Game over! {score} points!")
-                sys.exit()
 
-            for s in shots:
-                if s.collides(a):
-                    score += a.split()
-                    s.kill()
+        if paused:
+            continue
+        else:
+            for u in updatable:
+                u.update(dt)
 
-        screen.fill(pygame.Color(0, 0, 0))
+            for a in asteroids:
+                if a.collides(player):
+                    print(f"Game over! {score} points!")
+                    sys.exit()
 
-        for d in drawable:
-            d.draw(screen)
+                for s in shots:
+                    if s.collides(a):
+                        score += a.split()
+                        s.kill()
 
-        text = font.render(f"{score}", True, "#ffffff")
-        screen.blit(text, [0,0])
+            screen.fill(pygame.Color(0, 0, 0))
 
-        time_elapsed = time.gmtime(time.time() - start_time)
-        time_text = font.render(f"{time.strftime('%M:%S', time_elapsed)}", True, "#ffffff")
-        screen.blit(time_text, [200,0])
+            for d in drawable:
+                d.draw(screen)
 
-        pygame.display.flip()
-        dt = clock.tick(60) / 1000
+            text = font.render(f"{score}", True, "#ffffff")
+            screen.blit(text, [0,0])
+
+            time_elapsed = time.gmtime(time.time()-start_time)
+            time_text = font.render(f"{time.strftime('%M:%S', time_elapsed)}", True, UI_COLOUR)
+            screen.blit(time_text, [200,0])
+
+            pygame.display.flip()
+            dt = clock.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
